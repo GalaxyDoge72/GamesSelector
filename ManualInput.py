7e87b19975a3a42d72a9a143bf321d5793f542b2
GalaxyDoge72 <119555274+GalaxyDoge72@users.noreply.github.com>
2023-04-30 12:10:03 +1000

    Update ManualInput.py

--- ManualInput.py
+++ ManualInput.py
@@ -11,3 +11,4 @@ for index in range(gameCount):
 gameToPlay = random.randint(0, (gameCount - 1))
 selectedGame = gamesList[gameToPlay]
 print("The randomly chosen game is: " + selectedGame )
+input('Press any button to exit...')
